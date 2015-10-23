0f9655381d5edca491e2bc48f3124b2277b97f29
Nicolas PROCUREUR <nprocureur@gmail.com>
2015-10-23 10:59:55 +0200

    Add passport authentication

--- config/policies.js
+++ config/policies.js
@@ -26,8 +26,11 @@ module.exports.policies = {
   *                                                                          *
   ***************************************************************************/
 
-  // '*': true,
+  '*': ['passport', 'bearerAuth'],
 
+  'auth': {
+    '*': ['passport']
+  }
   /***************************************************************************
   *                                                                          *
   * Here's an example of mapping some policies to run before a controller    *
