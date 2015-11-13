c8c1830ab2d0d9bcd92ff26a72abacab3a8b6c59
JulienTi <julien.tisserant@y-nov.com>
2015-11-13 09:59:53 +0100

    Ajout frontend pour socket

--- api/controllers/LogController.js
+++ api/controllers/LogController.js
@@ -6,6 +6,60 @@
  */
 
 module.exports = {
-	
+
+  index: function (req, res) {
+
+    Log.find({user: req.user.id}).populate('lock').populate('user').exec(function (err, logs) {
+      if(err) return res.json(401, {err: 'Logs not retrieved:'+err});
+
+      return res.json(logs);
+    });
+  },
+
+  create: function (req, res) {
+    var user = req.user || false;
+
+    var params = _.extend(req.query || {}, req.params || {}, req.body || {}, {user: user.id});
+
+    Log.create(params).exec(function (err, newLog) {
+
+      if(err) return res.json(401, {err: 'Log not created:'+err});
+
+
+      Log.findOne({id: newLog.id}).populate('lock').exec(function(err, log){
+
+        if(err) return res.json(403, {err: 'forbidden'});
+        if(!log) return res.json(401, {err: 'Log could not be created'});
+
+        console.log(log);
+        return res.json({
+          log: log
+        });
+
+      });
+    });
+  },
+
+  destroy: function (req,res) {
+    var id = req.param('id');
+    if (!id) return res.send("No id specified.",500);
+
+
+    Log.find(id, function (err, log) {
+      if (err) return res.sender(err, 500);
+      if (!log) return res.send("Log " + id + " not found", 404);
+
+
+      Log.destroy(id, function (err) {
+        if (err) return res.send(err, 500);
+
+
+        return res.json({
+          log: 'Log destroyed'
+        });
+      });
+    });
+  }
 };
+
 
--- api/policies/authenticated.js
+++ api/policies/authenticated.js
@@ -1,7 +1,7 @@
 module.exports = function authenticated(req, res, next) {
   var token = req.headers.authorization || false;
   if(!token) {
-    return res.json(401, {user: 'user should be authenticated'});
+    return res.json(401, {user: 'invalid token'});
   }
   User.findOne({token: token}, function (err, user) {
     if(err || !user) return res.json(401, {err: 'user should be authenticated'});
