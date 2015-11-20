e6d1d299799c0e0eb58f266c2204a280ec55568a
Nicolas PROCUREUR <nprocureur@gmail.com>
2015-11-20 10:59:16 +0100

    Add log creation on lock update.

--- api/controllers/LockController.js
+++ api/controllers/LockController.js
@@ -11,6 +11,10 @@ module.exports = {
 
     User.findOne({id: req.user.id}).populate('locks').exec(function (err, user) {
       if(err) return res.json(401, {err: 'Locks not retrieved:'+err});
+      if(req.isSocket){
+        //Lock.subscribe(req, _.pluck(user.locks,'id'));
+        Lock.watch(req.socket);
+      }
 
       return res.json(user.locks);
     });
@@ -21,6 +25,16 @@ module.exports = {
 
     var params = _.extend(req.query || {}, req.params || {}, req.body || {}, {users: user.id});
 
+    if(req.isSocket && req.body.lock){
+      console.log('im a socket');
+      //console.log('im a socket', req.body, req.body.lock);
+      //var params = req.body.lock.lock;
+      var params = _.extend(req.body.lock.lock, {users: user.id});
+      console.log('p', params);
+    }
+    console.log('my', params);
+
+
     Lock.create(params).exec(function (err, newLock) {
 
       if(err) return res.json(401, {err: 'Lock not created:'+err});
@@ -30,6 +44,11 @@ module.exports = {
 
         if(err) return res.json(403, {err: 'forbidden'});
         if(!lock) return res.json(401, {err: 'Lock could not be created'});
+
+        console.log('publish');
+        Lock.publishCreate(lock, req);
+
+        Log.create({lock: lock.id, user: req.user.id});
 
         return res.json({
             lock: lock
@@ -96,6 +115,33 @@ module.exports = {
 
          if (err) return res.send(err, 500);
 
+         state = lock[0].state ? '1' : '0';
+
+         var http = require("http");
+
+         options = {
+           host: '192.168.240.1',
+           port: 80,
+           path: '/arduino/digital/13/'+state,
+           method: 'GET'
+
+         };
+
+         var ws_data = "";
+         var ws_request =  http.request(options, function(ws_response){
+             ws_response.on('error',function(e){ console.log(e.message);});
+             ws_response.on('data',function(chunk){ ws_data += chunk });
+             ws_response.on('end',function(chunk){ console.log('end'); });
+             console.log('ee', ws_data);
+         });
+
+         ws_request.write(ws_data);
+         ws_request.end();
+
+         Log.create({lock: lock.id, user: req.user.id});
+
+
+         //Lock.publishUpdate(lock.id, lock, req);
 
          return res.json({
            lock: lock
